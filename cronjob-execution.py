a7daa488ee01a844c3eb7a8af6970b55ad46ca2a
AvinashSubhash <avinashsubhash19@gmail.com>
2025-06-07 08:29:16 +0000

    cronjob working

---
+++ cronjob-execution.py
@@ -0,0 +1,23 @@
+import top_300_nse_tickers
+import os
+
+def identify_stocks():
+    final_buy_list = []
+    error_list = []
+    for ticker in top_300_nse_tickers.top_300_nse_tickers:
+        output=os.popen(f"curl -s http://signal-engine-service:8000/{ticker} | jq -r .buy").read()
+        if "true" not in output and "false" not in output:
+            error_list.append(ticker)
+        if "true" in output:
+            print(f"{ticker}")
+            final_buy_list.append(ticker)
+
+    return [final_buy_list,error_list]
+
+def trigger_email(list_data):
+    print("hello")
+
+if __name__ == "__main__":
+    list_data = identify_stocks()
+    if len(list_data[0])>0 or len(list_data[1])>0 or True:
+        trigger_email(list_data)
---
+++ deploy_project.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Build and push image to repository
+docker build -t kingaiva/signal-engine:dev . --no-cache
+docker image push kingaiva/signal-engine:dev
+
+# Delete old deployment and deploy the signal engine server
+kubectl delete deployment signal-engine --ignore-not-found
+kubectl apply -f signal-engine-deployment.yaml
+
+# Delete the old service and deploy the signal engine service
+kubectl delete service signal-engine-service --ignore-not-found
+kubectl apply -f signal-engine-service.yaml
+
+# Delete and recreate configmap for cronjob
+kubectl delete configmap cronjob-config --ignore-not-found
+kubectl create configmap cronjob-config --from-file=top_300_nse_tickers.py --from-file=cronjob-execution.py
+
+# delete and recreate the cronjob
+kubectl delete cronjob signal-check-cronjob --ignore-not-found
+kubectl apply -f signal-check-cronjob.yaml
+
+# create a manual cronjob and check functioning
+kubectl create job signal-check-manual --from=cronjob/signal-check-cronjob

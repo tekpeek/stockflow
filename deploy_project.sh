71ee0080e6f94abe73fe8d774c21794509fb0ec7
AvinashSubhash <avinashsubhash19@gmail.com>
2025-06-16 17:13:04 +0000

    enable ingress with TLS

--- deploy_project.sh
+++ deploy_project.sh
@@ -81,6 +81,9 @@ kubectl apply -f kubernetes/deployments/stockflow-controller-deployment.yaml
 # Verifying if the stockflow-controller is in running status
 check_deployment "stockflow-controller"
 
+# Sudo privilege task - one time task
+# cat kubernetes/configmaps/traefik-config.yaml > /var/lib/rancher/k3s/server/manifests/traefik-config.yaml
+
 # Apply the clusterIssuer
 kubectl apply -f kubernetes/services/stockflow-clusterissuer.yaml
 

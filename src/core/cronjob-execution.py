2b96f67dba8156dc7e27e520f0acd5ea37cee4f7
AvinashSubhash <avinashsubhash19@gmail.com>
2025-06-29 10:49:17 +0000

    update api path

--- src/core/cronjob-execution.py
+++ src/core/cronjob-execution.py
@@ -6,7 +6,7 @@ def identify_stocks():
     final_buy_list = []
     error_list = []
     for ticker in top_500_nse_tickers.top_500_nse_tickers:
-        output=os.popen(f"curl -s http://signal-engine-service:8000/{ticker} | jq -r .").read().strip()
+        output=os.popen(f"curl -s http://signal-engine-service:8000/api/{ticker} | jq -r .").read().strip()
         signals = os.popen(f"echo '{output}' | jq -r .signals").read().strip()
         strength = os.popen(f"echo '{output}' | jq -r .strength").read().strip()
         reasons = os.popen(f"echo '{output}' | jq -r .reason").read().strip()

5a98aacdd0127c6f0a118f5ff56ca9e9e8e23684
AvinashSubhash <avinashsubhash19@gmail.com>
2025-09-17 17:57:32 +0000

    Update market_intel server

--- src/api/market_intel.py
+++ src/api/market_intel.py
@@ -9,6 +9,7 @@ import os
 import numpy as np
 from openai import AsyncOpenAI
 import asyncio
+from pydantic import BaseModel
 
 # Configure logging to print to stdout
 logging.basicConfig(
@@ -19,22 +20,16 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+class Item(BaseModel):
+    prompt: str
+
 # Get values from environment variables (Kubernetes ConfigMap/Secret)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-def convert_bools_to_strings(data):
-    if isinstance(data, dict):
-        return {k: convert_bools_to_strings(v) for k, v in data.items()}
-    elif isinstance(data, list):
-        return [convert_bools_to_strings(item) for item in data]
-    elif isinstance(data, (bool, np.bool_)):  # Handle both Python bool and NumPy bool
-        return str(data)
-    return data
-
 market_intel = FastAPI()
 
-@signal_engine.get("/health")
-def health_check():
+@market_intel.get("/health")
+async def health_check():
     if MAINTENANCE_STATUS == "on":
         return JSONResponse({"status": "Maintenance mode is enabled"})
     time_stamp = datetime.datetime.now(datetime.UTC)
@@ -43,31 +38,13 @@ def health_check():
             "timestamp": f"{time_stamp}"
     })
 
-@signal_engine.get("/chat")
-def get_stock_data(
-    stock_id: str,
-    interval: str = DEFAULT_INTERVAL,
-    period: int = DEFAULT_PERIOD,
-    window: int = DEFAULT_WINDOW,
-    num_std: float = DEFAULT_NUM_STD
-):
+@market_intel.post("/chat")
+async def push_prompt(item: Item):
+    return item
     if MAINTENANCE_STATUS == "on":
         return JSONResponse({"status": "Maintenance mode is enabled"})
     logging.info("Triggering signal-engine for "+stock_id)
     logging.info(f"Strategy Values: interval: {interval}, period: {period}, window: {window}, num_std: {num_std}")
-    if stock_id.endswith(".NS"):
-        try:
-            return_data = sf.calculate_final_signal(stock_id,interval,period,window,num_std)
-            if return_data is None:
-                logger.error("Return data is None")
-                return JSONResponse({"error": "No data returned from signal calculation"})
-            return JSONResponse(return_data)
-        except Exception as e:
-            logger.error(f"Error: {str(e)}")
-            return JSONResponse({"error": f"Failed to process stock data: {str(e)}"})
-    else:
-        logger.warning(f"Invalid format: {stock_id}")
-        return JSONResponse({"error": "Incorrect Stock ID. Stock ID must end with .NS"})
 
 if __name__ == "__main__":
     logger.info("Starting up market-intel-engine server")

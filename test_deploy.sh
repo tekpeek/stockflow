3ee62907de9c29a8a19626a9e8f57e6eeb6d9c3c
AvinashSubhash <avinashsubhash19@gmail.com>
2025-10-04 09:42:46 +0000

    maintenance api test

--- test_deploy.sh
+++ test_deploy.sh
@@ -25,10 +25,18 @@ fi
 
 # Dummy Signal Generation Test
 dummy_signal=$(curl -s -X GET https://tekpeek.duckdns.org/api/RELIANCE.NS | jq -r .buy)
-if [[ "$dummy_signal" != "true" ]] || [[ "$dummy_signal" != "false" ]]; then
-    log_message "ERROR" "API Error! Endpoint [/api] endpoint not working."
+if [[ "$dummy_signal" != "true" ]] && [[ "$dummy_signal" != "false" ]]; then
+    log_message "ERROR" "API Error! Endpoint [/api] not working."
 else
     log_message "INFO" "Endpoint [/api] check completed."
+fi
+
+# Maintenance API Check
+maintenance_status=$(curl -s -X GET https://tekpeek.duckdns.org/api/admin/maintenance/status | jq -r .status)
+if [[ "$maintenance_status" != "on" ]] && [[ "$maintenance_status" != "off" ]]; then
+    log_message "ERROR" "API Error! Endpoint [/api/admin/maintenance/status] not working."
+else
+    log_message "INFO" "Endpoint [/api/admin/maintenance/status] check completed."
 fi
 
 log_message "INFO" "Test Deployment Execution Completed."

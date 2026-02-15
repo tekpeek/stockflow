9603761370c47540cc6f648d061ff61dcfb5ca82
AvinashSubhash <avinashsubhash19@gmail.com>
2026-02-15 17:22:54 +0000

    [skip actions] enable helm deployment in dev environment

--- stockflow/deploy_helm.sh
+++ stockflow/deploy_helm.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
-rm -r files/*
-cp ../src/core/discovery-engine.py ./files/discovery-engine.py
-cp ../src/core/cronjob-execution.py ./files/cronjob-execution.py
-cp ../src/core/healthcheck-execution.py ./files/healthcheck-execution.py
-cp ../src/core/smtp_email_trigger.py ./files/smtp_email_trigger.py
+rm -rf files/*
+cp -r ../src/core/*.py ./files/
 cp ../src/prompts/market_analysis_prompt.txt ./files/market_analysis_prompt.txt
-cp ../src/templates/email-template.html ./files/email-template.html
+cp ../templates/email-template.html ./files/email-template.html
 cp ../update_tickers/EQUITY_L.csv ./files/EQUITY_L.csv
 
-helm upgrade --install stockflow . -n dev2 --dry-run
-helm upgrade --install stockflow . -n dev2
+export OPENAI_API_KEY=$1
+export SMTP_PASSWORD=$2
+export API_KEY=$3
+export NAMESPACE=$4
+
+helm lint . --set namespace=$NAMESPACE
+
+if [ "$NAMESPACE" == "default" ]; then
+    helm upgrade stockflow --install . -n $NAMESPACE \
+        --set openaiApiKey=$OPENAI_API_KEY \
+        --set smtpPassword=$SMTP_PASSWORD \
+        --set apiKey=$API_KEY \
+        --set namespace=$NAMESPACE
+else
+    helm upgrade stockflow --install . -n $NAMESPACE \
+        --set openaiApiKey=$OPENAI_API_KEY \
+        --set smtpPassword="$SMTP_PASSWORD" \
+        --set apiKey=$API_KEY \
+        --set namespace=$NAMESPACE \
+        --set apiPrefix="/$NAMESPACE"
+fi
+
+rm -r files/*
+
+
+# Execution command
+### ./deploy_helm.sh "$OPENAI_API_KEY" "$SMTP_PASSWORD" "$API_KEY" "dev"

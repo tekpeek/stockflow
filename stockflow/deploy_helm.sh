0a904ae9e3d49ded77e3ff05fa7579e629b0d120
AvinashSubhash <avinashsubhash19@gmail.com>
2026-02-16 14:43:14 +0000

    test deployment

--- stockflow/deploy_helm.sh
+++ stockflow/deploy_helm.sh
@@ -23,8 +23,10 @@ export OPENAI_API_KEY=$1
 export SMTP_PASSWORD=$2
 export API_KEY=$3
 export NAMESPACE=$4
+export IMAGE_VERSION=$5
 log_message "INFO" "Setting environment variables"
 log_message "INFO" "NAMESPACE: $NAMESPACE"
+log_message "INFO" "IMAGE_VERSION: $IMAGE_VERSION"
 
 helm lint . --set namespace=$NAMESPACE
 log_message "INFO" "Linting helm chart completed."
@@ -34,14 +36,16 @@ if [ "$NAMESPACE" == "default" ]; then
         --set openaiApiKey=$OPENAI_API_KEY \
         --set smtpPassword=$SMTP_PASSWORD \
         --set apiKey=$API_KEY \
-        --set namespace=$NAMESPACE
+        --set namespace=$NAMESPACE \
+        --set imageVersion=$IMAGE_VERSION
 else
     helm upgrade stockflow --install . -n $NAMESPACE \
         --set openaiApiKey=$OPENAI_API_KEY \
         --set smtpPassword="$SMTP_PASSWORD" \
         --set apiKey=$API_KEY \
         --set namespace=$NAMESPACE \
-        --set apiPrefix="/$NAMESPACE"
+        --set apiPrefix="/$NAMESPACE" \
+        --set imageVersion=$IMAGE_VERSION
 fi
 
 rm -rf files/

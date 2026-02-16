ea5d90fd110af5f4323cd444a61dea8476cd3026
AvinashSubhash <avinashsubhash19@gmail.com>
2026-02-16 14:34:22 +0000

    test deployment

--- stockflow/deploy_helm.sh
+++ stockflow/deploy_helm.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
-ls -al
+
+log_message(){
+    local time=$(date +%d-%m-%y-%T)
+    local type=$1
+    local message=$2
+    echo "[$time]----[$type]---- $message"
+}
+
+log_message "INFO" "Starting deployment"
+
 rm -rf files/*
 mkdir -p files
+log_message "INFO" "Files directory cleaned"
+log_message "INFO" "Copying files to files directory"
 cp -r ../src/core/*.py files/
 cp ../src/prompts/market_analysis_prompt.txt files/market_analysis_prompt.txt
 cp ../templates/email-template.html files/email-template.html
 cp ../update_tickers/EQUITY_L.csv files/EQUITY_L.csv
+log_message "INFO" "Files copied to files directory"
 
 export OPENAI_API_KEY=$1
 export SMTP_PASSWORD=$2
 export API_KEY=$3
 export NAMESPACE=$4
+log_message "INFO" "Setting environment variables"
+log_message "INFO" "NAMESPACE: $NAMESPACE"
 
 helm lint . --set namespace=$NAMESPACE
+log_message "INFO" "Linting helm chart completed."
 
 if [ "$NAMESPACE" == "default" ]; then
     helm upgrade stockflow --install . -n $NAMESPACE \
@@ -30,7 +45,7 @@ else
 fi
 
 rm -rf files/
-
+log_message "INFO" "Files directory cleaned. Deployment completed."
 
 # Execution command
 ### ./deploy_helm.sh "$OPENAI_API_KEY" "$SMTP_PASSWORD" "$API_KEY" "dev"
